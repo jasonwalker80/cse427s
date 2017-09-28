a07d1c266fd477d5b47cfa8e98009aed21adff1a
Jason Walker <jwalker@genome.wustl.edu>
2017-09-27 19:44:42 -0500

    Bob question 1 and question 2.

--- hw4/problem2/SentimentPartitionerTest.java
+++ hw4/problem2/SentimentPartitionerTest.java
@@ -44,14 +44,9 @@ public class SentimentPartitionTest {
  		/*
 		 * TODO implement
 		 */          
-		result = spart.getPartition(new Text("love"), new IntWritable(1), 3);
-		assertEquals(0, result);
-		
 		result = spart.getPartition(new Text("deadly"), new IntWritable(1), 3);
-		assertEquals(1, result);
 		
-		result = spart.getPartition(new Text("zodiac"), new IntWritable(1), 3);
-		assertEquals(2, result);
+		assertEquals(0, result);
 	}
 
 }
