e9cd559b7a43b0907bee16f39bcbb41253de5408
bskowron23 <bob.skowron@gmail.com>
2017-11-12 17:32:38 -0600

    initial commit of CountJPGs.py
    For problem 3

---
+++ hw9/CountJPGs.py
@@ -0,0 +1,22 @@
+import sys
+from pyspark import SparkContext
+
+if __name__ == "__main__":
+	if len(sys.argv) < 1:
+		print >> sys.stderr, "Usage: countjpgs <file>"
+		exit(-1)
+	
+	# get spark context
+	sc = SparkContext()
+
+	# read file into spark and count jpgs
+	jpgCount = sc.textFile(sys.argv[1]) \
+		.map(lambda line: line.split(" ")) \
+		.filter(lambda fields: "jpg" in fields[6]) \
+		.count()
+
+	# print results
+	print jpgCount
+
+	# stop spark
+	sc.stop()
