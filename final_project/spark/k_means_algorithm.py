334df654e28c2b0555f14dc504919f30a6d5a69f
keon6kim <33881342+keon6kim@users.noreply.github.com>
2017-12-07 14:25:16 -0600

    Debugged up to line 100

--- final_project/spark/k_means_algorithm.py
+++ final_project/spark/k_means_algorithm.py
@@ -70,17 +70,17 @@ if __name__ == "__main__":
 		#takeSample(withReplacement, number) is a spark function that returns some subset of RDD in a list
 		centroid_list.append(data.takeSample(False,1)) 
 	# Could probably write some function that takes in some measure of geographical scale as parameter
+
 	#store centroid_list in RDD to output to HDFS
 	centroids_RDD = sc.parallelize(centroid_list)
-	#instead of randomly picking the initial centroids
 	
 	#First iteration outside the loop
 	# map (K,V) = ( cluster #, (lat, long) )
 	data = data.map(lambda p: (closestPoint(p,centroid_list), p) )	
 	#Run iterations until convergence
-	exit = False
+	stop = False
 	convergeDist = 0.1
-	while exit is False:
+	while stop is False:
 		#create new RDD to store sums of RDDs
 		#map (K,V) = [ cluster # ,(point, 1)]
 		#reduceByKey (sumReducer) to produce (K,V) = [cluster #, ( pointSum ,number of points in cluster = N) ]
@@ -89,11 +89,11 @@ if __name__ == "__main__":
 		#sort by cluster number in ascending order
 		#map (V) = new_centroid
 		#NOTE: the size of this RDD = k, therefore we can collect(), whose resulting list's indexes will correspond to cluster number
-		sums = data.map(lambda cluster,p: (cluster, (p, 1)) )\
-			.reduceByKey(add)\
-			.map(lambda cluster,sums: (cluster, (sums[0][0]/sums[1],sums[0][1]/sums[1] )) )\
+		sums = data.map(lambda line: (line[0], (line[1], 1)) )\
+			.reduceByKey(lambda line1,line2: (line1[0]+line2[0],line1[1]+line2[1],line1[2]+line2[2]) )\
+			.map(lambda line: (line[0], (line[1][0]/line[1][2],line[1][1]/line[1][2] )) )\
 			.sortByKey(True)\
-			.map(lambda cluster,new_centroid: new_centroid)
+			.map(lambda line: line[1])
 		#Get new centroid list
 		new_centroid_list = sums.collect()
 		#Check for convergence
@@ -107,28 +107,28 @@ if __name__ == "__main__":
 				converges = False
 		#If converged, exit loop, otherwise continue
 		if converges is True:
-			exit = True
+			stop = True
 		else:
 			centroid_list = new_centroid_list
 			# If not converging, do another iteration for : map (K,V) = ( cluster #, (lat, long) )
-			data = data.map(lambda cluster,p: (closestPoint(p,centroid_list), p) )	
+			data = data.map(lambda line: (closestPoint(line[1],centroid_list), line[1]) )	
 			centroids_RDD = sc.parallelize(centroid_list)
 			continue
 	
 	# Transform (cluster #, (lat, long) ) --> (cluster #, lat_of_centroid, long_of_centroid, radius_of_cluster (= max_distance) ,distance_from_centroid_to_point, lat, long)
 	#	Step 1: It's difficult to get the masximum distance (=radius) of each cluster. Let's start by creating some new RDD = (cluster, distance_from_centroid_to_point)
-	intermerdiate_RDD = data.map(lambda cluster,p: (cluster, GreatCircleDistance(p,centroid_list[(int) cluster]) ))
+	intermerdiate_RDD = data.map(lambda line: (line[0], GreatCircleDistance(line[1],centroid_list[(int) line[0]]) ))
 	#	Step 2: Create k new RDDs (in a list of RDDs) by filtering out by cluster number so each new RDD only contains distances for 1 cluster
 	maxDistances = list ()
 	for i in range (0,k):
-		maxDistances.append(intermerdiate_RDD.filter(lambda cluster,distance: (cluster==i) ))
+		maxDistances.append(intermerdiate_RDD.filter(lambda line: (line[0]==i) ))
 	#	Step 3: Sort By V = distances
 		maxDistances[i] = maxDistances[i].sortBy(lambda line: line[1])
 	#	Step 4: Take only the first element of the RDD and store them to a list of maximum distances in order of the cluster number
 		maxDistances[i] = maxDistances[i].take(1)[0]
 
 	#	Step 5: data = data.map (cluster #, lat_of_centroid, long_of_centroid, radius_of_cluster (= max_distance), distance_from_centroid_to_point, lat, long)
-	data = data.map (lambda cluster, p: (cluster, centroid_list[(int) cluster][0], centroid_list[(int) cluster][1], maxDistances[(int) cluster], GreatCircleDistance(p,centroid_list[(int) cluster]), p[0], p[1]) )\
+	data = data.map (lambda line: (line[0], centroid_list[(int) line[0]][0], centroid_list[(int) line[0]][1], maxDistances[(int) line[0]], GreatCircleDistance(line[1],centroid_list[(int) line[0]]), line[1][0], line[1][1]) )\
 		
 	#save file to HDFS: provide output path
 	data.saveAsTextFile(sys.argv[2])
